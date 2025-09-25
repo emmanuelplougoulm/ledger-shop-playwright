422a002787020060c6d38cd53923d25c55841411
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-09-25 17:37:28 +0200

    refactor(modularity): add component folder into repo

---
+++ src/pages/components/Footer.ts
@@ -0,0 +1,5 @@
+import { Locator, Page } from '@playwright/test'
+
+export class Footer {
+  constructor(private page: Page) {}
+}
---
+++ src/pages/components/Navigation.ts
@@ -0,0 +1,5 @@
+import { Locator, Page } from '@playwright/test'
+
+export class Navigation {
+  constructor(private page: Page) {}
+}
