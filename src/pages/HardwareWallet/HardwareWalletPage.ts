43cfc740047c573748b9b241ce19156d411fcb6b
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-09-26 11:51:16 +0200

    refactor: reorganise repo structure for lisibility

--- src/pages/HardwareWallet/HardwareWalletPage.ts
+++ src/pages/HardwareWallet/HardwareWalletPage.ts
@@ -1,7 +1,7 @@
 import { Locator, Page, expect } from '@playwright/test'
 
-import { LEDGER_URLS } from '../../constants/URLS'
-import { WebsiteBanner } from './components/WebsiteBanner'
+import { LEDGER_URLS } from '../../../constants/URLS'
+import { WebsiteBanner } from '../components/WebsiteBanner/WebsiteBanner'
 
 interface UserData {
   email: string
