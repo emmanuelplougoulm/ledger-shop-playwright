09a7e5b88eaaf6ca86b1603110700415f8d10b3f
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-09-26 18:17:43 +0200

    refactor: use newly created POM in checkout workflow

--- __tests__/e2e/checkout/checkout.spec.ts
+++ __tests__/e2e/checkout/checkout.spec.ts
@@ -1,45 +1,55 @@
 import { expect, test } from '@playwright/test'
 
-import { HardwareWalletPage } from '../../../pages/HardwareWallet/HardwareWalletPage'
+import { HardwareWalletPage } from '../../../pages/HardwareWalletPage/HardwareWalletPage'
+import { InformationPage } from '../../../pages/InformationPage/InformationPage'
+import { SideDrawer } from '../../../pages/__components/SIdeDrawer/SideDrawer'
 
-test.describe('Ledger Hardware Wallet Checkout Flow', () => {
-  test('should complete entire checkout flow', async ({ page }) => {
-    const checkoutPage = new HardwareWalletPage(page)
+const UserData = {
+  email: 'victor@gmail.com',
+  firstName: 'victor',
+  lastName: 'toto',
+  address: '2, rue de la paix',
+  postalCode: '75002',
+  city: 'Paris',
+  phone: '0687654567',
+}
+
+test.describe('Hardware Wallet Checkout Flow', () => {
+  test('should complete an entire checkout flow', async ({ page }) => {
+    const hwPage = new HardwareWalletPage(page)
+    const sideDrawer = new SideDrawer(page)
+    const infoPage = new InformationPage(page)
 
     await test.step('1. Navigate to Hardware Wallets page', async () => {
-      await checkoutPage.goto()
-      await expect(checkoutPage.page).toHaveURL(/hardware-wallet/)
+      await hwPage.goto()
+      await expect(page).toHaveURL(/hardware-wallet/)
     })
 
     await test.step('2. Add Ledger Flex to cart', async () => {
-      await checkoutPage.addFlexToCart()
+      await hwPage.addFlexToCart()
     })
 
     await test.step('3. Select No Coverage option', async () => {
-      await checkoutPage.selectNoCoverageOption()
+      await sideDrawer.selectNoCoverageOption()
     })
 
-    await test.step('4. Proceed to checkout', async () => {
-      await checkoutPage.proceedToCheckout()
-      await expect(checkoutPage.page).toHaveURL(/checkouts/)
+    await test.step('4. Continue to checkout', async () => {
+      await sideDrawer.continueToCheckout()
+      await expect(page).toHaveURL(/information/)
     })
 
-    // await test.step('5. Fill shipping information form', async () => {
-    //   await checkoutPage.fillInfosForm()
-    // })
-
-    await test.step('7. Proceed to shipping', async () => {
-      await checkoutPage.proceedToShipping()
-      await expect(checkoutPage.page).toHaveURL(/shipping/)
+    await test.step('5. Fill shipping information form', async () => {
+      await infoPage.fillShippingForm(UserData)
     })
 
-    await test.step('6. Select standard shipping option', async () => {
-      await checkoutPage.selectStandardShipping()
+    await test.step('6. Proceed to shipping', async () => {
+      await infoPage.proceedToShipping()
+      await expect(page).toHaveURL(/shipping/)
     })
 
-    await test.step('8. Proceed to payment', async () => {
-      await checkoutPage.proceedToPayment()
-      await expect(checkoutPage.page).toHaveURL(/payment/)
+    await test.step('7. Proceed to payment', async () => {
+      await infoPage.proceedToPayment()
+      await expect(page).toHaveURL(/payment/)
     })
   })
 })
